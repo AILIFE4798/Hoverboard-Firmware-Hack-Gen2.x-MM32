37feddbb58a65278b4761bd5f18a48b2926cf989
AILIFE4798 <142502122+AILIFE4798@users.noreply.github.com>
2024-02-03 15:53:52 +0800

    added self hold

--- HoverBoardMindMotion/Src/main.c
+++ HoverBoardMindMotion/Src/main.c
@@ -10,10 +10,12 @@
 #define HALL2LED
 
 s32 main(void){
+	//enable gpio clock
 	RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOA, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOB, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOC, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOD, ENABLE);
+	//LED output
 	GPIO_InitTypeDef GPIO_InitStruct;
   GPIO_InitStruct.GPIO_Pin = LEDRPIN;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
@@ -23,7 +25,13 @@ s32 main(void){
 	GPIO_Init(LEDGPORT, &GPIO_InitStruct);
 	GPIO_InitStruct.GPIO_Pin = LEDBPIN;
 	GPIO_Init(LEDBPORT, &GPIO_InitStruct);
-	
+	//latch output
+	GPIO_InitStruct.GPIO_Pin = LATCHPIN;
+	GPIO_Init(LATCHPORT, &GPIO_InitStruct);
+	//bz output
+	GPIO_InitStruct.GPIO_Pin = BZPIN;
+	GPIO_Init(BZPORT, &GPIO_InitStruct);
+	//hall input
 	GPIO_InitStruct.GPIO_Pin = HALLAPIN;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_Init(HALLAPORT, &GPIO_InitStruct);
@@ -31,24 +39,38 @@ s32 main(void){
 	GPIO_Init(HALLBPORT, &GPIO_InitStruct);
 	GPIO_InitStruct.GPIO_Pin = HALLCPIN;
 	GPIO_Init(HALLCPORT, &GPIO_InitStruct);
-	
+	//button input
+	GPIO_InitStruct.GPIO_Pin = BTNPIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_FLOATING;
+	GPIO_Init(BTNPORT, &GPIO_InitStruct);
+	//systick config
 	DELAY_Init();
+	//wait while release button
+	while(GPIO_ReadInputDataBit(BTNPORT, BTNPIN)) {
+		GPIO_WriteBit(BZPORT, BZPIN, 1);
+		DELAY_Ms(1);
+		GPIO_WriteBit(BZPORT, BZPIN, 0);
+		DELAY_Ms(1);
+	}
+		
   while(1) {
 		#ifdef HALL2LED
+		//rotating led
     GPIO_WriteBit(LEDRPORT, LEDRPIN, GPIO_ReadInputDataBit(HALLAPORT, HALLAPIN));
 		GPIO_WriteBit(LEDGPORT, LEDGPIN, GPIO_ReadInputDataBit(HALLBPORT, HALLBPIN));
 		GPIO_WriteBit(LEDBPORT, LEDBPIN, GPIO_ReadInputDataBit(HALLCPORT, HALLCPIN));
-		DELAY_Ms(2);		
+		DELAY_Ms(5);		
 		#else
+		//blink in sequence
 		GPIO_WriteBit(LEDRPORT, LEDRPIN, 1);
 		GPIO_WriteBit(LEDBPORT, LEDBPIN, 0);
-		DELAY_Ms(200);
+		DELAY_Ms(500);
 		GPIO_WriteBit(LEDGPORT, LEDGPIN, 1);
 		GPIO_WriteBit(LEDRPORT, LEDRPIN, 0);
-		DELAY_Ms(200);
+		DELAY_Ms(500);
 		GPIO_WriteBit(LEDBPORT, LEDBPIN, 1);
 		GPIO_WriteBit(LEDGPORT, LEDGPIN, 0);
-		DELAY_Ms(200);
+		DELAY_Ms(500);
 		#endif
   }
 	
--- HoverBoardMindMotion/Src/pinout.h
+++ HoverBoardMindMotion/Src/pinout.h
@@ -1,13 +1,31 @@
+//file for setting custom pinout before autodetect
+//pinout is for Layout2.8.1
+
+//3LED
 #define LEDRPIN GPIO_Pin_12
 #define LEDGPIN GPIO_Pin_2
 #define LEDBPIN GPIO_Pin_3
 #define LEDRPORT GPIOA
 #define LEDGPORT GPIOD
 #define LEDBPORT GPIOD
-
+//hall sensor
 #define HALLAPIN GPIO_Pin_13
 #define HALLBPIN GPIO_Pin_14
 #define HALLCPIN GPIO_Pin_15
 #define HALLAPORT GPIOC
 #define HALLBPORT GPIOC
 #define HALLCPORT GPIOC
+//self hold
+#define LATCHPIN GPIO_Pin_2
+#define LATCHPORT GPIOB
+//power button
+#define BTNPIN GPIO_Pin_5
+#define BTNPORT GPIOB
+//buzzer
+#define BZPIN GPIO_Pin_9
+#define BZPORT GPIOB
+
+
+
+
+
